de96b4d5b0f5eb6a8f6fac6b6cf79dde78a4835c
Nabin Purbey <nabinpurbey03@hotmail.com>
2025-11-29 21:32:38 +0545

    new deployment added

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 echo "<h5 style='color: green'>Jenkins works well.</h5>";
 echo "<h1 style='color: green'>Build and Deploy Successful</h1>";
+echo "<h1 style='color: green'>New Deployment Successful</h1>";
