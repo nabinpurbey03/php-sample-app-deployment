135900318da963c4bac3e2f52024ddfe40ba8426
Nabin Purbey <nabinpurbey03@hotmail.com>
2025-11-30 11:29:15 +0545

    updated index to Last Deployment

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
-echo "<h5 style='color: green'>Jenkins works well.</h5>";
-echo "<h1 style='color: green'>Build and Deploy Successful</h1>";
-echo "<h1 style='color: green'>New Deployment Successful</h1>";
-echo "<h1 style='color: green'>New Deployment New again</h1>";
-echo "<h1 style='color: green'>New Deployment New again again</h1>";
+echo "<h5 style='color: green'>Last Deployment.</h5>";
