1193f68e1e87f3f549fe868d6a7fdbf8eb6c6767
Nabin Purbey <nabinpurbey03@hotmail.com>
2025-12-01 10:19:49 +0545

    update  cicd

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 echo "<h5 style='color: red'>Last Deployment.</h5>";
 echo "<h5 style='color: blue'>Server Monitoring Test</h5>";
 echo "<h1 style='color: green'>CI/CD Works Fine, Pushing on MAIN Branch</h1>";
+echo "<h1 style='color: green'>New update</h1>";
