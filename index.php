318a13c6bc9103bac8787fc24c418cbb1f24829b
Nabin Purbey <nabinpurbey03@hotmail.com>
2025-12-01 10:00:07 +0545

    chicking cicd

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-echo "<h5 style='color: green'>Last Deployment.</h5>";
-echo "<h5 style='color: green'>Server Monitoring Test</h5>";
+echo "<h5 style='color: red'>Last Deployment.</h5>";
+echo "<h5 style='color: blue'>Server Monitoring Test</h5>";
+echo "<h1 style='color: green'>CI/CD Works Fine, Pushing on MAIN Branch</h1>";
