556e9b17365a3e0ade46748f9c0a5ec676631c77
Nabin Purbey <112373792+nabinpurbey03@users.noreply.github.com>
2025-12-05 11:27:25 +0545

    Update

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
-echo "<h5 style='color: red'>Last Deployment.</h5>";
-echo "<h5 style='color: blue'>Server Monitoring Test</h5>";
-echo "<h1 style='color: green'>CI/CD Works Fine, Pushing on MAIN Branch</h1>";
-echo "<h1 style='color: green'>New update</h1>";
+echo "<h1 style='color: red'>Last Deployment.</h1>";
+
